b351c0f3d85f014b3f37fb92521c66033f797c26
Serhii Ozeran <ozeransm@gmail.com>
2023-03-02 19:05:19 +0300

    add modal Book a Table to Restaurant’s details

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-
-import "./slick/slick";
+import './slick/slick';
 import './js/slaider';
 import './js/modal';
 import './js/slick-tab';
 import './js/slick';
 import './js/current';
 import './js/mobile-menu';
+import './js/boock';

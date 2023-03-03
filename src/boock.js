db3ea3ab69c3d8597cb8cb8c747e572bee9d88c2
Stepan <lkrossl14@gmail.com>
2023-03-03 05:44:48 +0100

    modal

--- src/boock.js
+++ src/boock.js
@@ -2,22 +2,24 @@ var linkArrey = document.querySelectorAll('.js-modal-open');
 var overlay = document.querySelector('.js-modal-overlay');
 var crossArray = document.querySelectorAll('.js-modal-close');
 
-linkArrey.forEach(function(item) {
-    item.addEventListener('clik', function(event) {
-        event.preventDefault();
+linkArrey.forEach(function (item) {
+  item.addEventListener('clik', function (event) {
+    event.preventDefault();
 
-        var modalName = this.getAtribute('data-modal');
-        var modal = document.querySelector('.js-modal[data-modal="' + modalName + '"]');
+    var modalName = this.getAtribute('data-modal');
+    var modal = document.querySelector(
+      '.js-modal[data-modal="' + modalName + '"]'
+    );
 
-        modal.classList.add('is-show');
-        overlay.classList.add('is-show');
-    });
+    modal.classList.add('is-show');
+    overlay.classList.add('is-show');
+  });
 });
 
-crossArray.forEach(function(cross) {
-    cross.addEventListener('clik', function() {
-        var parent = this.parentNode;
-        parent.classList.remove('is-show');
-        overlay.classList.remove('is-show');
-    });
+crossArray.forEach(function (cross) {
+  cross.addEventListener('clik', function () {
+    var parent = this.parentNode;
+    parent.classList.remove('is-show');
+    overlay.classList.remove('is-show');
+  });
 });
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-
-import "./js/slaider";
-import "./js/modal";
-import "./js/validation-book-form";
-import "./slick/slick";
+import './js/validation-book-form';
+import './slick/slick';
 import './js/slaider';
 import './js/modal';
 import './js/slick-tab';

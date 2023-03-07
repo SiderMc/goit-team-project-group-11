b623bc5fe33c2ededb7f38491e99251419e761a2
SiderMc <104320647+SiderMc@users.noreply.github.com>
2023-03-07 02:42:09 +0000

    Deploying to gh-pages from @ SiderMc/goit-team-project-group-11@5d2c4784265a400971ac6dc5fca2488b85fa3ed0 🚀

---
+++ assets/page-room-3fdfb5bb.js
@@ -0,0 +1,5 @@
+import"./modulepreload-polyfill-ec808ebb.js";$(".slider").slick({appendArrows:".arrows-container",prevArrow:`<div class="prev-arrow-container"><svg class="prev-arrow" xmlns="http://www.w3.org/2000/svg" width="9" height="16" fill="none">
+    <path  stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 15 1 8l7-7"/>
+    </svg></div>`,nextArrow:`<div class="next-arrow-container"><svg class="next-arrow" xmlns="http://www.w3.org/2000/svg" width="9" height="16" fill="none">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 15 7-7-7-7"/>
+    </svg></div>`,infinite:!0,speed:300,slidesToShow:1,variableWidth:!0,swipe:!0,touchMove:!0});

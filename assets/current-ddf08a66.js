e6397884a86664650cab0936630af0206303a535
SiderMc <104320647+SiderMc@users.noreply.github.com>
2023-03-07 03:46:49 +0000

    Deploying to gh-pages from @ SiderMc/goit-team-project-group-11@6bc030523f03701ad2ce547999eb1daa2599ab7e 🚀

---
+++ assets/current-ddf08a66.js
@@ -0,0 +1,1 @@
+const o=window.location.pathname,a=document.querySelectorAll(".header-nav-link");a.forEach(n=>{o===n.pathname&&n.classList.add("current")});console.log(o);
--- assets/page-room-e0083977.js
+++ assets/page-room-e0083977.js
@@ -1,4 +1,4 @@
-import"./modulepreload-polyfill-ec808ebb.js";$(".slider").slick({appendArrows:".arrows-container",prevArrow:`<div class="prev-arrow-container"><svg class="prev-arrow" xmlns="http://www.w3.org/2000/svg" width="9" height="16" fill="none">
+import"./mobile-menu-341ab0f2.js";import"./book-room-d20b1518.js";import"./prices-room-af6146c5.js";$(".slider").slick({appendArrows:".arrows-container",prevArrow:`<div class="prev-arrow-container"><svg class="prev-arrow" xmlns="http://www.w3.org/2000/svg" width="9" height="16" fill="none">
     <path  stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 15 1 8l7-7"/>
     </svg></div>`,nextArrow:`<div class="next-arrow-container"><svg class="next-arrow" xmlns="http://www.w3.org/2000/svg" width="9" height="16" fill="none">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 15 7-7-7-7"/>
